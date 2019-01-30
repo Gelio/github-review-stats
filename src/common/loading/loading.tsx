4a3e35c133ee8027a6191521820b0554016f80ab
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-30 21:23:52 +0100

    remove unnecessary files

--- src/common/loading/loading.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export const Loading: React.StatelessComponent = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-Loading.defaultProps = {
-  children: 'Loading...'
-};
