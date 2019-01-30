3bd0e089c19d01d9494943fdbc9c7ab4300cd2b4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-01-30 20:26:47 +0100

    add environment variables for app secret and ID

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,20 @@
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
+if (!clientId) {
+  console.error(
+    'Client ID is not specified. Make sure to fill in the environment variables.',
+  );
+}
+
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+if (!clientSecret) {
+  console.error(
+    'Client secret is not specified. Make sure to fill in the environment variables.',
+  );
+}
+
 export const config = {
   GITHUB_API_URL: 'https://api.github.com/',
-  GITHUB_CLIENT_ID: 'c671c642cc5094466c10',
+  GITHUB_CLIENT_ID: clientId,
+  GITHUB_CLIENT_SECRET: clientSecret,
 };
