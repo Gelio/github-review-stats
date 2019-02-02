8c64c07381acf579ac3ccfe40f781a213ec48e4c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-02-02 21:29:34 +0100

    Updates

---
+++ precache-manifest.bc396243f7db9e521155ffa34341a24e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b189877f9a04dbc13fa8",
+    "url": "/github-review-stats/static/js/main.b189877f.chunk.js"
+  },
+  {
+    "revision": "dd2655277d34b02d036f",
+    "url": "/github-review-stats/static/js/1.dd265527.chunk.js"
+  },
+  {
+    "revision": "40704383e2ba5ba6128c",
+    "url": "/github-review-stats/static/js/runtime~main.40704383.js"
+  },
+  {
+    "revision": "7da857e430e39840ca80003cf8d81a29",
+    "url": "/github-review-stats/index.html"
+  }
+];
