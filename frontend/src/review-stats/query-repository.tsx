48587bd4ae82c5ef03d376eb77a9420b1e16f17e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-08-17 08:11:17 +0200

    use fetching multiple PR pages

--- frontend/src/review-stats/query-repository.tsx
+++ frontend/src/review-stats/query-repository.tsx
@@ -1,94 +1,52 @@
 import { CircularProgress } from '@material-ui/core';
-import { format } from 'date-fns/esm';
-import gql from 'graphql-tag';
-import React, { FunctionComponent } from 'react';
-import { Query } from 'react-apollo';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { useApolloClient } from 'react-apollo';
 
 import { ReviewStatsInputs, PullRequest } from './types';
 import { MetricPickerWithCharts } from './metric-picker-with-charts';
-
-const GET_REPOSITORY_INFO_QUERY = gql`
-  query GetRepositoryInfo(
-    $firstPrs: Int!
-    $firstReviews: Int!
-    $query: String!
-  ) {
-    search(first: $firstPrs, type: ISSUE, query: $query) {
-      nodes {
-        ... on PullRequest {
-          number
-          permalink
-          title
-          updatedAt
-          changedFiles
-          additions
-          deletions
-          author {
-            login
-          }
-          reviews(first: $firstReviews) {
-            nodes {
-              author {
-                login
-              }
-              comments {
-                totalCount
-              }
-              publishedAt
-              state
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+import { fetchPrs } from './fetching/fetch-prs';
 
 interface QueryRepositoryProps {
   queryData: ReviewStatsInputs;
 }
 
-interface QueryResponse {
-  search: {
-    nodes: PullRequest[];
-  };
-}
-
 export const QueryRepository: FunctionComponent<QueryRepositoryProps> = ({
   queryData,
 }) => {
-  const queryVariables = {
-    firstPrs: 100,
-    firstReviews: 100,
-    query: createSearchQueryString(queryData),
-  };
+  const [pullRequests, setPullRequests] = useState<PullRequest[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const apolloClient = useApolloClient();
+
+  useEffect(() => {
+    fetchPrs(apolloClient, queryData)
+      .then(setPullRequests)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, [apolloClient, queryData]);
+
+  if (loading) {
+    return (
+      <div>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (error) {
+    console.error('Error while fetching the query', error);
+    return (
+      <div>
+        <p>Error!</p>
+      </div>
+    );
+  }
+
+  console.log(pullRequests);
 
   return (
     <div>
-      <Query<QueryResponse>
-        query={GET_REPOSITORY_INFO_QUERY}
-        variables={queryVariables}
-      >
-        {({ loading, error, data }) => {
-          if (loading) {
-            return <CircularProgress />;
-          }
-          if (error || !data) {
-            console.error('Error while fetching the query', error);
-            return <p>Error!</p>;
-          }
-
-          return <MetricPickerWithCharts pullRequests={data.search.nodes} />;
-        }}
-      </Query>
+      <MetricPickerWithCharts pullRequests={pullRequests} />
     </div>
   );
 };
-
-const formatGitHubDateTime = (date: Date) =>
-  [format(date, 'yyyy-MM-dd'), 'T', format(date, 'HH:mm:ss'), 'Z'].join('');
-
-const createSearchQueryString = (queryData: ReviewStatsInputs) =>
-  `type:pr repo:${queryData.repository} updated:${formatGitHubDateTime(
-    queryData.fromDate,
-  )}..${formatGitHubDateTime(queryData.toDate)} sort:updated-desc`;
