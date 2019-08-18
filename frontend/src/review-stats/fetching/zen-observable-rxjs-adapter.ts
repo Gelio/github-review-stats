f3119655c9f5a895aaaf3c1cb7d87bbc9ba84137
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2019-08-18 12:49:11 +0200

    add zenObservableToRxJS adapter with tests

---
+++ frontend/src/review-stats/fetching/zen-observable-rxjs-adapter.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+
+import ZenObservable from 'zen-observable';
+
+/**
+ * Adapts a Zen Observable to an RxJS observable
+ * @param zenObservable
+ */
+export const zenObservableToRxJs = <T>(zenObservable: ZenObservable<T>) =>
+  new Observable<T>((subscriber) => {
+    const subscription = zenObservable.subscribe({
+      next: (val) => subscriber.next(val),
+      complete: () => subscriber.complete(),
+      error: (err) => subscriber.error(err),
+    });
+
+    return () => subscription.unsubscribe();
+  });
